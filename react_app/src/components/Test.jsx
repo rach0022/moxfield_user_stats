0aea663eaa71966690af9437c7318a9fc4de9e43
RaviC Rachamalla <rach0022@algonquinlive.com>
2024-06-17 11:02:40 -0400

    FRONTEND: implemented bulma notification and visual progress timer

--- react_app/src/components/Test.jsx
+++ react_app/src/components/Test.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import BulmaNotification from './Bulma/BulmaNotification.jsx';
 
 export default function Test() {
+
+    const testContentForNotification = (
+        <p className={'has-text-weight-bold'}>The is the notification test content</p>
+    );
+
     return (
-        <h1 className="title">Test Component</h1>
+        <>
+            <h1 className="title">Test Component</h1>
+            <BulmaNotification message={testContentForNotification} type="is-primary" />
+        </>
     );
 }
